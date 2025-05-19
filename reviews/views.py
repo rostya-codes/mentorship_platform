8051962ffc24380032271f918f273609b25cbc05
Rostya <rostyamudrik@proton.me>
2025-05-19 16:05:24 +0200

    Mentor's profile with reviews is done

--- reviews/views.py
+++ reviews/views.py
@@ -2,13 +2,20 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.views import View
 
+from reviews.models import Review
+
 User = get_user_model()
 
 
 class MentorProfileView(View):
     def get(self, request, mentor_id, *args, **kwargs):
         mentor = User.objects.get(pk=mentor_id)
-        return render(request, 'reviews/mentor-profile.html', {'mentor': mentor})
+        reviews = Review.objects.filter(mentor=mentor)
+        return render(
+            request,
+            'reviews/mentor-profile.html',
+            {'mentor': mentor, 'reviews': reviews},
+        )
 
 
 class LeaveReviewView(View):
