ef848fdb0b9fa2e845310b80aa27422c89d5e7d8
Rostya <rostyamudrik@proton.me>
2025-06-02 18:08:42 +0100

    Models validations added

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+
+from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import ForeignKey
+from django.utils import timezone
+from tomlkit import comment
+
+from schedule.models import Slot
 
 
 class Review(models.Model):
@@ -25,3 +32,25 @@ class Review(models.Model):
 
     def __str__(self):
         return f'{self.pk} M: {self.mentor.username} U: {self.user.username} | stars: {self.rating}'
+
+    def clean(self):
+        if 0 > self.rating > 5:
+            raise ValidationError('Rating must be 0 - 5')
+
+        if self.rating <= 2 and len(self.comment) < 15:
+            raise ValidationError('Your rating is too small you you must writ a comment (min 15 symbols)')
+
+        review = Review.objects.filter(user=self.user, slot=self.slot).exclude(pk=self.pk)
+        if review.exists():
+            raise ValidationError('Your review for this slot already exists.')
+
+        if self.slot:
+            if not self.slot.user == self.user:
+                raise ValidationError('You can only leave a review for a slot you attended.')
+
+        now = timezone.now()
+        event_datetime = datetime.combine(self.slot.date, self.slot.time)
+        if timezone.is_aware(now):  # Add TIME_ZONE
+            event_datetime = timezone.make_aware(event_datetime, timezone.get_current_timezone())
+        if event_datetime > now:
+            raise ValidationError('You cannot leave a review before the slot has ended.')
--- schedule/models.py
+++ schedule/models.py
@@ -1,18 +1,16 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
-
-User = get_user_model()
 
 
 class Slot(models.Model):
     mentor = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         limit_choices_to={'is_mentor': True},
         related_name='mentor_slots'
     )
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='user_bookings')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, related_name='user_bookings')
     date = models.DateField()
     time = models.TimeField()
     is_booked = models.BooleanField(default=False)
@@ -22,6 +20,10 @@ class Slot(models.Model):
 
     def __str__(self):
         return f"{self.pk} {self.mentor.username} - {self.date} {self.time} ({'Booked' if self.is_booked else 'Free'})"
+
+    def clean(self):
+        if self.is_booked and not self.user or not self.is_booked and self.user:
+            raise ValidationError('is_booked and user existing is connected fields.')
 
 
 class BookingLog(models.Model):
